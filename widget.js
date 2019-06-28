cdbd92f30a8fa336757829d065d1ec5662d5b84c
Khalid Esserhir <khalid.esserhir@gmail.com>
2019-06-28 22:21:34 +0100

    widget

---
+++ widget.js
@@ -0,0 +1,7 @@
+var frame = document.createElement('iframe');
+                frame.src="https://houria-ch.github.io/widget/";
+                var Wstyle= document.getElementById('theWidget');
+                Wstyle.appendChild(frame);
+                frame.style.height="180px";
+                frame.style.border="none";
+                frame.style.width="100%";
